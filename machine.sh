448cccdadef1a4c2c496cd96af24a906dad0b1e9
Daisy <71977928+anhbtit80@users.noreply.github.com>
2025-03-06 23:08:23 +0700

    Update machine.sh

--- machine.sh
+++ machine.sh
@@ -2,18 +2,6 @@
 #This script isn't Advanced 
 #Author Anaz
 
-#making ngrok directory 
-mkdir ngrok
-
-#changing directory to ngrok
-cd ngrok
-
-#removing all existing files 
-rm * 
-
-#removing all existing folders 
-rm -r *
-
 curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
 	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
 	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
@@ -22,12 +10,8 @@ curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
 	&& sudo apt install ngrok
 
 #asking ngrok auth token
-echo " Go to ngrok.io in any browser & signin or signup, copy the ngrok auth token and paste here, ngrok token only, Readme - https://github.com/developeranaz/cloudshell-novnc-automation   Ngrok Auth token: "
-read input_token
-echo "You entered: $input_token"
-./ngrok authtoken $input_token
 
-#orgin-repository : https://github.com/developeranaz/cloudshell-novnc-automation
+ngrok authtoken 22b8lmaOjoNz75rVr5GQSBnXSfX_4zXe75MrZJb1XbbQkNAFj
 
 #updating system
 sudo apt update -y
@@ -39,4 +23,4 @@ sudo apt-get install screen -y
 #pushing docker ubuntu desktop using screen (you can change resolution from below code) 
 screen -d -m docker run -p 8080:80 -e RESOLUTION=1920x1080 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
 
-./ngrok http 8080
+ngrok http 8080
